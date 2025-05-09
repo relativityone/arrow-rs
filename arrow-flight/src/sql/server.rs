69b55585d885df102898ef75c6cf706ef4bb8e74
Alex Wilcoxson <alex.wilcoxson@relativity.com>
2025-05-09 09:28:39 -0500

    chore: pick sql

--- arrow-flight/src/sql/server.rs
+++ arrow-flight/src/sql/server.rs
@@ -602,7 +602,21 @@ where
         &self,
         request: Request<FlightDescriptor>,
     ) -> Result<Response<FlightInfo>, Status> {
-        let message = Any::decode(&*request.get_ref().cmd).map_err(decode_error_to_status)?;
+        let message: Any =
+            match Message::decode(&*request.get_ref().cmd).map_err(decode_error_to_status) {
+                Ok(msg) => msg,
+                Err(_) => {
+                    return self
+                        .get_flight_info_fallback(
+                            Command::Unknown(Any {
+                                type_url: "".to_string(),
+                                value: bytes::Bytes::new(),
+                            }),
+                            request,
+                        )
+                        .await
+                }
+            };
 
         match Command::try_from(message).map_err(arrow_error_to_status)? {
             Command::CommandStatementQuery(token) => {
@@ -666,7 +680,20 @@ where
         request: Request<Ticket>,
     ) -> Result<Response<Self::DoGetStream>, Status> {
         let msg: Any =
-            Message::decode(&*request.get_ref().ticket).map_err(decode_error_to_status)?;
+            match Message::decode(&*request.get_ref().ticket).map_err(decode_error_to_status) {
+                Ok(msg) => msg,
+                Err(_) => {
+                    return self
+                        .do_get_fallback(
+                            request,
+                            Any {
+                                type_url: "".to_string(),
+                                value: bytes::Bytes::new(),
+                            },
+                        )
+                        .await
+                }
+            };
 
         match Command::try_from(msg).map_err(arrow_error_to_status)? {
             Command::TicketStatementQuery(command) => self.do_get_statement(command, request).await,
@@ -710,10 +737,28 @@ where
         // we wrap this stream in a `Peekable` one, which allows us to peek at
         // the first message without discarding it.
         let mut request = request.map(PeekableFlightDataStream::new);
-        let cmd = Pin::new(request.get_mut()).peek().await.unwrap().clone()?;
+        let cmd = Pin::new(request.get_mut())
+            .peek()
+            .await
+            .ok_or_else(|| Status::invalid_argument("Empty stream"))?
+            .clone()?;
 
-        let message =
-            Any::decode(&*cmd.flight_descriptor.unwrap().cmd).map_err(decode_error_to_status)?;
+        let message = match Any::decode(&*cmd.flight_descriptor.unwrap().cmd)
+            .map_err(decode_error_to_status)
+        {
+            Ok(msg) => msg,
+            Err(_) => {
+                return self
+                    .do_put_fallback(
+                        request,
+                        Any {
+                            type_url: "".to_string(),
+                            value: bytes::Bytes::new(),
+                        },
+                    )
+                    .await
+            }
+        };
         match Command::try_from(message).map_err(arrow_error_to_status)? {
             Command::CommandStatementUpdate(command) => {
                 let record_count = self.do_put_statement_update(command, request).await?;
